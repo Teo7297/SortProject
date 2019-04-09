fe3cbbe889091b53191fd2897011ad8a4216be6d
Teo7297 <teocava7297@gmail.com>
2019-04-09 10:39:44 +0200

    aggiunta classe per Selection sort

---
+++ SortProject/src/Selection.java
@@ -0,0 +1,38 @@
+
+public class Selection {
+	private static int min;
+	
+	
+	
+	public static void main(String args[]) {
+		int[] vector = {7, 8 ,3,2,5,6,9,1,4};
+		vector = sort(vector);
+		
+		for(int h = 0; h < vector.length; h++)
+			System.out.println(vector[h]);
+		
+	}
+
+	public static int[] sort(int[] vector) {
+		min = 0;
+		for (int j = 0; j < vector.length; j++) {
+			min = j;
+			for (int i = j+1; i < vector.length; i++) {
+				if (vector[i] < vector[min]) {
+					min = i;
+				}
+			}
+			exch(vector, min, j);
+		}
+		return vector;
+	}
+
+	
+	public static int[] exch(int[] vector, int first, int second) {
+		int temp;
+		temp = vector[first];
+		vector[first] = vector[second];
+		vector[second] = temp;
+		return vector;
+	}
+}
