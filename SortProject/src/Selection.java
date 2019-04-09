91b679c8c4f70c0c3e6ef046829cf9d626c94568
teoca <teoca@DESKTOP-AGTKPSG>
2019-04-09 22:50:58 +0200

    j

--- SortProject/src/Selection.java
+++ SortProject/src/Selection.java
@@ -4,15 +4,6 @@
 public class Selection {
 	private static int min;
 
-	public static void main(String args[]) {
-		int[] vector = { 7, 8, 3, 2, 5, 6, 9, 1, 4 }; // eliminare il main ed implementare generatore random per l'array
-		vector = sort(vector);
-
-		for (int h = 0; h < vector.length; h++)
-			System.out.println(vector[h]);
-
-	}
-
 	public static int[] sort(int[] vector) {
 		min = 0;
 		for (int j = 0; j < vector.length; j++) {
